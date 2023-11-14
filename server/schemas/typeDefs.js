57116e77717345091c983c81c9298cff93f8ecb5
BellaAdrien <Iadrien2020@fau.edu>
2023-11-14 17:15:59 -0500

    save21

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,14 +26,22 @@ authors: [String]
   type Query {
     me: User
   }
+  input bookInput{
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    saveBook(book: bookInput!): User
+    
+    removeBook(bookId: String!): User
+    
   }
 `;
 
